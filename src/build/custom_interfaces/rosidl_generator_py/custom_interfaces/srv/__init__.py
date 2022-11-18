11a62c548d231daf53b7b2e4af2565cc8952e260
Parth Patel <parth.pmech@gmail.com>
2022-11-18 15:12:25 -0500

    Add Group Project 1 submission zip code file

--- src/build/custom_interfaces/rosidl_generator_py/custom_interfaces/srv/__init__.py
+++
@@ -1,1 +0,0 @@
-from custom_interfaces.srv._find_joint_states import FindJointStates  # noqa: F401
--- src/build/rrbot_gazebo/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/parth/Desktop/ros2_ws/src/install/custom_interfaces/share/custom_interfaces/package.sh"
